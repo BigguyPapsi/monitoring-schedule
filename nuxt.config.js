a0a64996ae18b3872508e529dc3f0155ec0136b9
ThanouPap <83503745+BigguyPapsi@users.noreply.github.com>
2026-05-26 18:12:36 +0700

    KNN

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,10 +4,10 @@ export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
 
-  server: {
-    port: 3000, // default: 3000
-    host: "0.0.0.0"
-  },
+  // server: {
+  //   port: 3000, // default: 3000
+  //   host: "0.0.0.0"
+  // },
 
   // Target: https://go.nuxtjs.dev/config-target
   target: "static",
