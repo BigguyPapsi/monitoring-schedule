16c864dfc1556a329d63681ec461a8d47212e319
ThanouPap <83503745+BigguyPapsi@users.noreply.github.com>
2026-05-26 10:53:12 +0700

    fix base url

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: "static",
   router: {
-    base: "/monitoring-schedule/",
+    base: process.env.NODE_ENV === "production" ? "/monitoring-schedule/" : "/",
   },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
