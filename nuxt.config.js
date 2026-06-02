67f639f14168a5c3200fe795b2443ea71f02091b
ThanouPap <83503745+BigguyPapsi@users.noreply.github.com>
2026-06-02 14:46:25 +0700

    deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,8 +44,6 @@ export default {
       },
     ],
   },
-
-  // Global CSS: https://go.nuxtjs.dev/config-css
   css: ["~/assets/styles/custom.css"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
